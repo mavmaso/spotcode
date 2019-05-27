4699246eb7aacc1a37a12d392c957ed5fb90cb2c
Marcos Soares <mavmaso@gmail.com>
2019-05-27 11:45:31 -0300

    Add dashboard and toolbar with style

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'home/index'
-  root to: 'home#index'
+
+  authenticated :user do
+    root to: "dashboard#index", as: :authenticated_root
+  end
+
+  unauthenticated :user do
+    root to: "home#index"
+  end
 end
