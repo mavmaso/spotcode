0a15541ea23cb02148c697b8e333afa254bbf929
Marcos Soares <mavmaso@gmail.com>
2019-05-27 15:07:47 -0300

    Add search bar with style

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-
+  
   authenticated :user do
     root to: "dashboard#index", as: :authenticated_root
+    resources :search, only: [:index, :new], as: :searches
   end
 
   unauthenticated :user do
